a2ba7b9fc07cdcccc5be3b43ec6b8cba968cadc5
DZN1KOR <danasivasankararao.chandika@in.bosch.com>
2024-08-20 15:35:15 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -96,7 +96,7 @@ TEST(string_calculator_add_when_passed_negative_numbers,throws_an_exception_list
   //Act
   int actualSum=calculator.Add(input);
   //Assert
-  ASSERT_THROW(actualSum,"Negatives not allowed: -2,-4");
+  ASSERT_THROW(Negatives not allowed, "-2,-4");
 }
 
 
