c28516cd99bd4034e7a407b46d7bcd90393f2528
DZN1KOR <danasivasankararao.chandika@in.bosch.com>
2024-08-20 15:25:22 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -63,4 +63,43 @@ TEST(string_calculator_add_when_passed_two_comma_delimited_numbers,returns_their
   ASSERT_EQ(actualSum,expectedsum);
 }
 
+TEST(string_calculator_add_when_passed_multiple_comma_delimited_numbers,returns_their_sum){
+ //Arrange
+  StringCalculator calculator;
+ 
+  string input="1,2,3";
+  int expectedsum=6;
+  //Act
+  int actualSum=calculator.Add(input);
+  //Assert
+  ASSERT_EQ(actualSum,expectedsum);
+}
+
+TEST(string_calculator_add_when_delimited_with_newline_and_comma,returns_the_sum_based_on_that_delimiter){
+ //Arrange
+  StringCalculator calculator;
+ 
+  string input="//;\n1;2";
+  int expectedsum=3;
+  //Act
+  int actualSum=calculator.Add(input);
+  //Assert
+  ASSERT_EQ(actualSum,expectedsum);
+}
+
+TEST(string_calculator_add_when_passed_negative_numbers,throws_an_exception_listing_invalid_values){
+ //Arrange
+  StringCalculator calculator;
+ 
+  string input="1,-2,-4,5"";
+  int expectedsum=3;
+  //Act
+  int actualSum=calculator.Add(input);
+  //Assert
+  ASSERT_EQ(actualSum,expectedsum);
+}
+
+
+
+
 
