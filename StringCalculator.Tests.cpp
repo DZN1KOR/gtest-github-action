93363a7e04b0b84644598e06089fa825aef9eb42
DZN1KOR <danasivasankararao.chandika@in.bosch.com>
2024-08-20 15:16:02 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -15,10 +15,27 @@ TEST(string_calculator_add_when_passed_a_single_number,returns_0_for_empty_strin
 TEST(string_calculator_add_when_passed_a_single_number,returns_0_for_empty_zeo){
  //Arrange
   StringCalculator calculator;
-  string input="0";
-  int expectedsum=0;
+ int i=0;
+ for(i=0; i<1000; i++)
+  {
+  string input="i";
+  int expectedsum=i;
   //Act
   int actualSum=calculator.Add(input);
   //Assert
   ASSERT_EQ(actualSum,expectedsum);
 }
+}
+
+// TEST(string_calculator_add_when_passed_a_single_number,returns_1_for_empty_one){
+//  //Arrange
+//   StringCalculator calculator;
+ 
+//   string input="1";
+//   int expectedsum=1;
+//   //Act
+//   int actualSum=calculator.Add(input);
+//   //Assert
+//   ASSERT_EQ(actualSum,expectedsum);
+// }
+
